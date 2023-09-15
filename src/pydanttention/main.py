1fdad06512a5ff1065f181423a1f755620ffeada
Louis Maddox <louismmx@gmail.com>
2023-09-15 11:12:17 +0100

    feat: introduce Pydantic model

--- src/pydanttention/main.py
+++ src/pydanttention/main.py
@@ -1,21 +1,48 @@
 from __future__ import annotations
 
-from .prediction import predict, untok
+import numpy as np
+from pydantic import BaseModel
+
+from .constants import CHARS, MODEL
+from .sources import gpt, softmax, tokenize
 
 __all__ = ["ManualTransformer"]
 
 
-class ManualTransformer:
-    def __init__(self, report=False):
-        self.test = "aab" * 10
-        self.total, self.correct = 0, 0
+class ManualTransformer(BaseModel):
+    test: str = "aab" * 10
+    total: int = 0
+    correct: int = 0
+    report: bool = False
+
+    def __init__(self):
+        super().__init__()
+        self.run()
+
+    def run(self) -> None:
         for i in range(2, len(self.test) - 1):
             ctx = self.test[:i]
             expected = self.test[i]
             self.total += 1
-            if untok(predict(ctx)) == expected:
+            if self.untok(self.predict(ctx)) == expected:
                 self.correct += 1
-        if report:
+        if self.report:
             print(
                 f"ACCURACY: {self.correct / self.total * 100}% ({self.correct} / {self.total})",
             )
+
+    @staticmethod
+    def untok(tok):
+        return CHARS[tok]
+
+    def predict(self, s, report=True):
+        tokens = tokenize(s)[-5:]
+        logits = gpt(np.array(tokens), **MODEL)
+        probs = softmax(logits)
+        for i, tok in enumerate(tokens):
+            pred = np.argmax(probs[i])
+            if report:
+                print(
+                    f"{self.untok(tok)} ({tok}): next={self.untok(pred)} ({pred}) probs={probs[i]} logits={logits[i]}",
+                )
+        return np.argmax(probs[-1])
