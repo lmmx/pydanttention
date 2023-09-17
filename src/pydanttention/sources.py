c93a30149a64561915709cc88722ae512b1b1e91
Louis Maddox <louismmx@gmail.com>
2023-09-17 11:01:04 +0100

    style: shape comments as docstrings

--- src/pydanttention/sources.py
+++ src/pydanttention/sources.py
@@ -18,38 +18,34 @@ def softmax(x):
     return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
 
 
-# [m, in], [in, out], [out] -> [m, out]
 def linear(x, w, b):
+    """[m, in], [in, out], [out] -> [m, out]"""
     return x @ w + b
 
 
-# [n_q, d_k], [n_k, d_k], [n_k, d_v], [n_q, n_k] -> [n_q, d_v]
 def attention(q, k, v, mask):
+    """[n_q, d_k], [n_k, d_k], [n_k, d_v], [n_q, n_k] -> [n_q, d_v]"""
     return softmax(q @ k.T / np.sqrt(q.shape[-1]) + mask) @ v
 
 
-# [n_seq, n_embd] -> [n_seq, n_embd]
 def causal_self_attention(x, c_attn, c_proj):
+    """[n_seq, n_embd] -> [n_seq, n_embd]"""
     # qkv projections
     x = linear(x, **c_attn)  # [n_seq, n_embd] -> [n_seq, 3*n_embd]
-
     # split into qkv
     q, k, v = np.split(x, 3, axis=-1)  # [n_seq, 3*n_embd] -> 3 of [n_seq, n_embd]
-
     # causal mask to hide future inputs from being attended to
     causal_mask = (1 - np.tri(x.shape[0], dtype=x.dtype)) * -1e10  # [n_seq, n_seq]
-
     # perform causal self attention
     x = attention(q, k, v, causal_mask)  # [n_seq, n_embd] -> [n_seq, n_embd]
-
     # out projection
     x = linear(x, **c_proj)  # [n_seq, n_embd] @ [n_embd, n_embd] = [n_seq, n_embd]
 
     return x
 
 
-# [n_seq, n_embd] -> [n_seq, n_embd]
 def transformer_block(x, attn):
+    """[n_seq, n_embd] -> [n_seq, n_embd]"""
     x = x + causal_self_attention(x, **attn)
     # NOTE: removed ffn
     return x
